7054819eaa7b73752dc1ec3f6269224ea815a502
Lucassigma123 <lucass126@nycstudents.net>
2025-10-30 09:55:44 -0400

    Update .py

--- .py
+++ .py
@@ -1,27 +1,26 @@
 cart=[]
 cost=(0)
-item= {
+1= {
     "name":"pistol",
     "price":"$500",
     "department":"GUNS",
     "description":"very cool pew pew"
 }
-item1={
+2={
     "name":"ak47",
     "price":"$1000",
     "department":"GUNS",
     "description":"Fast fire rate pew pew"
 }
-item2={
+3={
     "name":"rocket launcher",
     "price":"$5000",
     "department":"explosive",
     "description":"boom boom boom"
 }
-print(item)
-print(item1)
-print(item2)
+print(1["name"],1["price"])
+print(2["name"],2["price"])
+print(3["name"],3["price"])
 while True:
     x= input("choose a gun to buy type no to stop buy and checkout")
-  
-
+    
