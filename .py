b2138240e51af06fb5481082b604d2041adf4e41
Lucassigma123 <lucass126@nycstudents.net>
2025-10-30 10:17:12 -0400

    Update .py

--- .py
+++ .py
@@ -1,26 +1,26 @@
 cart=[]
-cost=(0)
-1= {
-    "name":"pistol",
-    "price":"$500",
-    "department":"GUNS",
-    "description":"very cool pew pew"
-}
-2={
-    "name":"ak47",
-    "price":"$1000",
-    "department":"GUNS",
-    "description":"Fast fire rate pew pew"
-}
-3={
-    "name":"rocket launcher",
-    "price":"$5000",
-    "department":"explosive",
-    "description":"boom boom boom"
-}
-print(1["name"],1["price"])
-print(2["name"],2["price"])
-print(3["name"],3["price"])
+cost=0
+sigma=[
+    {
+        "name":"pistol",
+        "price":"$500",
+        "department":"GUNS",
+        "description":"very cool pew pew"
+    },
+    {
+        "name":"ak47",
+        "price":"$1000",
+        "department":"GUNS",
+        "description":"Fast fire rate pew pew"
+    },
+    {
+        "name":"rocket launcher",
+        "price":"$5000",
+        "department":"explosive",
+        "description":"boom boom boom"
+    }
+]
+
+
 while True:
     x= input("choose a gun to buy type no to stop buy and checkout")
-    
